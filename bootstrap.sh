8b945a1bf799dc40509e4ad9ae26da4d810c6820
Enrique Comba Riepenhausen <ecomba@patheleven.com>
2015-03-21 14:03:45 +0000

    updating if already available

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,9 @@ if ! command -v brew > /dev/null 2>&1; then
   echo "Okay, lets do this. First things first, we need to install homebrew in order to get all the stuff you need"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-  echo "Homebrew was already available... skipping"
+  echo "Homebrew was already available... skipping the installation"
+  echo "Let's update it instead! (you know you haven't updated in a while)"
+  brew update
 fi
 
 if ! command -v fish > /dev/null 2>&1 ; then
@@ -16,9 +18,17 @@ if ! command -v fish > /dev/null 2>&1 ; then
   chsh -s /usr/local/bin/fish
 else
   echo "Fish already installed! Nice one!"
+  echo "Upgrading (just in case)"
+  brew upgrade fish
 fi
 
 # HERE WE WILL START WITH THE ACTUALL INSTALLATION
-git clone git@github.com:ecomba/osx-setup.git "$HOME/.osx-setup"
-cd "$HOME/.osx-setup"
+if ! [ -d "$HOME/.osx-setup]; then
+  git clone git@github.com:ecomba/osx-setup.git "$HOME/.osx-setup"
+  cd "$HOME/.osx-setup"
+else
+  cd "$HOME/.osx-setup"
+  git pull
+fi
+
 fish setup.fish
