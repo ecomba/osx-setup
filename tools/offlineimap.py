7cc4350ff7149b14bba3c0fe0d56a2ed9e1d8242
Enrique Comba Riepenhausen <ecomba@patheleven.com>
2015-06-05 10:11:46 +0100

    Email: ecomba...

--- tools/offlineimap.py
+++ tools/offlineimap.py
@@ -2,13 +2,13 @@
 import re, subprocess
 def get_keychain_pass(account=None, server=None):
     params = {
-        'security': '/usr/bin/security',
+        'security': 'security',
         'command': 'find-internet-password',
         'account': account,
         'server': server,
-        'keychain': '/Users/sjl/Library/Keychains/login.keychain',
+        'keychain': '/Users/ecomba/Library/Keychains/login.keychain',
     }
-    command = "sudo -u sjl %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    command = "sudo -u ecomba %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
     output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
     outtext = [l for l in output.splitlines()
                if l.startswith('password: ')][0]
