e346a9d640ffd66148e17723a5d9029dd58b62f4
Enrique Comba Riepenhausen <ecomba@8thlight.com>
2016-01-30 15:13:33 +0000

    Adds fzf to the brews to install

--- mixtures/brews.fish
+++ mixtures/brews.fish
@@ -7,7 +7,7 @@ function ferment -d "Installs all the formulae needed"
                 'chruby' 'chruby-fish' 'elixir' 'erlang' 'go' 'groovy' \
                 'io' 'maven' 'mit-scheme' 'node' 'phantomjs' 'tmate' \
                 'ruby-build' 'mysql' 'postgresql' 'python3' 'jq' \
-                'boot2docker' 'docker' 'mutt' 'sqlite' 'ino'
+                'boot2docker' 'docker' 'mutt' 'sqlite' 'ino' 'fzf'
 
   for formula in $formulae
     if contains $formula $installed_formulae
@@ -17,4 +17,7 @@ function ferment -d "Installs all the formulae needed"
       brew install $formula
     end
   end
+
+  # Install fzf (fuzzy finder)
+  /usr/local/opt/fzf/install
 end
