dcd9dd03a8f47dc33931186e022a2b38a4336a45
Enrique Comba Riepenhausen <ecomba@8thlight.com>
2016-03-09 19:35:16 +0000

    Replaces pathogen with vim-plug

---
+++ vim/plugs.vim
@@ -0,0 +1,35 @@
+call plug#begin('~/.vim/plugged')
+  Plug 'dag/vim-fish'
+
+  Plug 'vim-airline/vim-airline'
+  Plug 'vim-airline/vim-airline-themes'
+
+  Plug 'chriskempson/base16-vim'
+  Plug 'nanotech/jellybeans.vim'
+
+  Plug 'roman/golden-ratio'
+
+  Plug 'tmux-plugins/vim-tmux'
+
+  Plug 'szw/vim-tags'
+  Plug 'dkprice/vim-easygrep'
+
+  Plug 'jplaut/vim-arduino-ino'
+  Plug 'mileszs/ack.vim'
+
+  Plug 'tpope/vim-surround'
+  Plug 'tpope/vim-endwise'
+  Plug 'tpope/vim-fugitive'
+  Plug 'tpope/vim-cucumber'
+
+  Plug 'kien/ctrlp.vim'
+  Plug 'scrooloose/nerdtree',         { 'on': 'NERDTreeToggle' }
+  Plug 'tfnico/vim-gradle',           { 'for': 'gradle' }
+  Plug 'vim-ruby/vim-ruby',           { 'for': 'ruby' }
+  Plug 'ecomba/vim-ruby-refactoring', { 'for': 'ruby' }
+  Plug 'tpope/vim-haml',              { 'for': 'haml' }
+  Plug 'kchmck/vim-coffee-script',    { 'for': 'coffee' }
+  Plug 'elixir-lang/vim-elixir',      { 'for': 'elixir' }
+  Plug 'andreimaxim/vim-io',          { 'for': 'io' }
+  Plug 'elzr/vim-json',               { 'for': 'json' }
+call plug#end()
--- vim/vim.fish
+++ vim/vim.fish
@@ -1,46 +1,11 @@
 function setup_vim -d "Installs my vim configuration and the plugins"
   rm -f "$HOME/.vimrc"
   ln -s "$HOME/.osx-setup/vim/vimrc" "$HOME/.vimrc"
-  _install_pathogen
-  _install_bundles
+  _install_vim_plug
 end
 
-function _install_pathogen
-  set -x -g vimbundledir "$HOME/.vim/bundle"
-  mkdir -p "$HOME/.vim/autoload" $vimbundledir
-  curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
-end
-
-function _install_bundles
-  set bundles 'https://github.com/mileszs/ack.vim.git' \
-              'https://github.com/bling/vim-airline' \
-              'https://github.com/kien/ctrlp.vim.git' \
-              'https://github.com/tpope/vim-cucumber.git' \
-              'https://github.com/elixir-lang/vim-elixir.git' \
-              'https://github.com/dag/vim-fish.git' \
-              'https://github.com/roman/golden-ratio.git' \
-              'https://github.com/tpope/vim-haml.git' \
-              'https://github.com/andreimaxim/vim-io.git' \
-              'https://github.com/scrooloose/nerdtree.git' \
-              'https://github.com/kien/rainbow_parentheses.vim.git' \
-              'https://github.com/vim-ruby/vim-ruby.git' \
-              'https://github.com/kchmck/vim-coffee-script.git' \
-              'https://github.com/tpope/vim-surround.git' \
-              'https://github.com/chriskempson/base16-vim.git' \
-              'https://github.com/tpope/vim-endwise.git' \
-              'https://github.com/ecomba/vim-ruby-refactoring.git' \
-              'https://github.com/tmux-plugins/vim-tmux.git' \
-              'https://github.com/elzr/vim-json.git' \
-              'https://github.com/nanotech/jellybeans.vim.git' \
-              'https://github.com/szw/vim-tags.git' \
-              'https://github.com/dkprice/vim-easygrep.git' \
-              'https://github.com/tpope/vim-fugitive.git' \
-              'https://github.com/jplaut/vim-arduino-ino.git' \
-              'https://github.com/tfnico/vim-gradle.git'
-
-  cd $vimbundledir
-  for bundle in $bundles
-    git clone $bundle
-  end
-  cd $HOME
+function _install_vim_plug
+  set -x -g plugdir "$HOME/.vim/plugged"
+  mkdir -p "$HOME/.vim/autoload" $plugdir
+  curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 end
